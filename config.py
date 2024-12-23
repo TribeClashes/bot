ac1731f03795abc31c98c52bda91e21a50808acd
Plummy <plummybeatsoff@gmail.com>
2024-12-23 17:10:22 +0100

    Added optional redis dsn

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     telegram_bot_token: SecretStr
+    redis_dsn: SecretStr | None = None
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
+from pydantic import SecretStr
 
 from bot.middlewares.logging import LoggingMiddleware
 from config import Config
@@ -12,8 +15,14 @@ config: Config = Config(_env_file=".env")
 
 
 def create_dispatcher() -> Dispatcher:
-    new_dispatcher: Dispatcher = Dispatcher()
+    redis_dsn: SecretStr | None = config.redis_dsn
 
+    if redis_dsn is None:
+        storage: MemoryStorage = MemoryStorage()
+    else:
+        storage: RedisStorage = RedisStorage.from_url(redis_dsn.get_secret_value())
+
+    new_dispatcher: Dispatcher = Dispatcher(storage=storage)
     new_dispatcher.update.outer_middlewarere.register(LoggingMiddleware("tribeclashes"))
 
     return new_dispatcher
